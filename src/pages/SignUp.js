dfea07265e8c614b91eaa6144a6ed61356dda39e
sapkotapaudel <yanapaudeli@gmail.com>
2023-03-08 17:01:21 -0500

    test

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,7 +8,7 @@ function SignUp() {
     <div>
         {/* Need to imoprt background image here, just couldn't figure out how :'( */}
     <section className="bg-gray h-screen">
-       
+       {/* Test */}
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0" >
       
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray ">
