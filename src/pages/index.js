e1e2f0f191c1d42fda2609dc01fc135c3f8d1d57
Prabin Basnet <prabinbasnet09@gmail.com>
2023-03-15 18:53:04 -0500

    solved login module not found

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,15 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { useState, useEffect, useContext} from 'react'
 import Dashboard from '../components/Dashboard'
-import Login from './login'
-import SignUp from './SignUp'
-import AppStatus from '../components/Dashboard'
 import { ActiveUser } from './_app.js'
-import { Button } from '@aws-amplify/ui-react'
-
-import Cookies from 'js-cookie'
 
 
 export default function Home({signOut}) {
   const activeUser = useContext(ActiveUser);
   const [groups, setGroups] = useState([]);
   const [users, setUsers] = useState(null);
-  const router = useRouter();
+
 
   useEffect(() => {
     const { group, users} = activeUser;
