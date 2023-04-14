7d43f857916d1f3004aaa37cf7108212b023928a
Prabin Basnet <prabinbasnet09@gmail.com>
2023-04-14 02:47:02 -0500

    resolved package.json for main

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState, useEffect, useContext } from 'react';
@@ -29,19 +28,4 @@ export default function Result() {
       </div>
     </div>
   ) : null;
-=======
-import React from "react";
-import Link from "next/link";
-
-export default function Result() {
-    return (
-        <div className="flex items-center justify-center">
-            <div className="w-3/4 px-2 sm:px-0">
-                <div className={`${"nav-body"}`}> 
-                    <p>Results</p>
-                </div>
-            </div>
-        </div>
-    )
->>>>>>> 9346c4296420f80a7de36a5863d6d1f94c71db5a
 }
