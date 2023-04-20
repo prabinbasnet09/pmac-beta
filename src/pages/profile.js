4ad0036a997be37d397e6121e6989825e9eb7e16
LockJaw140 <holdeninc@aol.com>
2023-04-20 02:41:11 -0500

    Updated profile page design

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -48,112 +48,115 @@ export default function Profile() {
   };
 
   return activeUser ? (
-    <div className='mt-10 flex items-center justify-center'>
-      <div className='bg-white shadow-2xl mt-10 mb-10 w-80 justify-center grid rounded-lg'>
-        <div className='flex justify-center rounded-lg mt-10'>
-          <Image src={Logo} className='w-[5rem] rounded-full' alt='ULM Logo' />
+    <div className='flex justify-center items-center'>
+      <div className='flex flex-wrap gap-5 items-center w-3/4 justify-evenly bg-white shadow-2xl py px-2 sm:px-0 rounded-lg'>
+        <div className='grid gap-y-4'>
+          <div className='flex justify-center items-center'>
+            <Image src={Logo} className='h-40 w-40 rounded-full' alt='ULM Logo' />
+          </div>
+          <div className='text-center rounded-lg'>
+            <p className='text-lg font-bold'>Profile</p>
+            <p>{activeUser.username}</p>
+            <p>{activeUser.name}</p>
+            <p>{activeUser.email}</p>
+          </div>
         </div>
-
-        <div className='rounded-lg text-center'>
-          <p className='text-lg font-bold'>Profile</p>
-          <p>{activeUser.username}</p>
-          <p>{activeUser.name}</p>
-          <p>{activeUser.email}</p>
+        <div className='flex justify-center'>
+          <form
+            className='justify-center w-60'
+            onSubmit={e => {
+              e.preventDefault();
+              handleProfileUpdate();
+            }}
+          >
+            <label
+              htmlFor='Update Name'
+              className='block text-center mb-2 text-lg font-medium text-gray-900 dark:text-black'
+            >
+              Update Name
+            </label>
+            <label
+              htmlFor='Name'
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'
+            >
+              Full Name
+            </label>
+            <input
+              type='text'
+              id='old_password'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder={activeUser.name}
+              required
+              onChange={e => {
+                e.preventDefault();
+                setName(e.target.value);
+              }}
+            />
+            <button
+              type='submit'
+              className='mt-4 border-2 border-black w-full bg-white text-black shadow-xl font-bold p-3 rounded-full'
+            >
+              Submit
+            </button>
+          </form>
         </div>
-
-        <form
-          className='justify-center mt-10 w-60'
-          onSubmit={e => {
-            e.preventDefault();
-            handleProfileUpdate();
-          }}
-        >
-          <label
-            htmlFor='Update Name'
-            className='block text-center mb-2 text-lg font-medium text-gray-900 dark:text-black'
-          >
-            Update Name
-          </label>
-          <label
-            htmlFor='Name'
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'
-          >
-            Full Name
-          </label>
-          <input
-            type='text'
-            id='old_password'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder={activeUser.name}
-            required
-            onChange={e => {
+        <div className='flex justify-center'>
+          <form
+            className='w-60'
+            onSubmit={e => {
               e.preventDefault();
-              setName(e.target.value);
+              handlePasswordUpdate();
             }}
-          />
-          <button
-            type='submit'
-            className='mt-4 border-2 border-black w-full bg-white text-black shadow-xl font-bold p-3 rounded-full'
           >
-            Submit
-          </button>
-        </form>
+            <label
+              htmlFor='Update Password'
+              className='block text-center mb-2 text-lg font-medium text-gray-900 dark:text-black'
+            >
+              Update Password
+            </label>
+            <label
+              htmlFor='Old Password'
+              className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'
+            >
+              Old Password
+            </label>
+            <input
+              type='password'
+              id='password'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder='•••••••••'
+              required
+              onChange={e => {
+                e.preventDefault();
+                setOldPassword(e.target.value);
+              }}
+            />
 
-        <form
-          className=' mt-10 w-60'
-          onSubmit={e => {
-            e.preventDefault();
-            handlePasswordUpdate();
-          }}
-        >
-          <label
-            htmlFor='Update Password'
-            className='block text-center mb-2 text-lg font-medium text-gray-900 dark:text-black'
-          >
-            Update Password
-          </label>
-          <label
-            htmlFor='Old Password'
-            className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'
-          >
-            Old Password
-          </label>
-          <input
-            type='password'
-            id='password'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder='•••••••••'
-            required
-            onChange={e => {
-              e.preventDefault();
-              setOldPassword(e.target.value);
-            }}
-          />
-
-          <label
-            htmlFor='New Password'
-            className='block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-black'
-          >
-            New Password
-          </label>
-          <input
-            type='password'
-            id='password'
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
-            placeholder='•••••••••'
-            required
-            onChange={e => {
-              e.preventDefault();
-              setNewPassword(e.target.value);
-            }}
-          />
-          <button
-            type='submit'
-            className='mt-4 mb-10 border-2 border-black w-full bg-white text-black shadow-xl font-bold p-3 rounded-full'
-          >
-            Submit
-          </button>
-        </form>
+            <label
+              htmlFor='New Password'
+              className='block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-black'
+            >
+              New Password
+            </label>
+            <input
+              type='password'
+              id='password'
+              className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'
+              placeholder='•••••••••'
+              required
+              onChange={e => {
+                e.preventDefault();
+                setNewPassword(e.target.value);
+              }}
+            />
+            <button
+              type='submit'
+              className='mt-4 mb-10 border-2 border-black w-full bg-white text-black shadow-xl font-bold p-3 rounded-full'
+            >
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   ) : null;
