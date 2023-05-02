cb2a9845d002bbd5f502d91fb59b56f13bda94a8
sapkotapaudel <yanapaudeli@gmail.com>
2023-05-02 01:22:12 -0400

    Icons TabBar

--- src/components/widgets/TabBar.js
+++ src/components/widgets/TabBar.js
@@ -27,23 +27,57 @@ export default function TabBar({ tabList }) {
       <div className='flex items-center justify-center'>
         {isSidebar ? (
           <nav className='fixed left-0 top-1/2 transform -translate-y-1/2 h-80vh bg-ulm_maroon w-10 px-4 py-20 rounded-r-3xl'>
-            <ul className='space-y-20 flex flex-col justify-center items-center'>
+            <ul className='space-y-10 flex flex-col justify-center items-center'>
               {tabs &&
                 tabs.map((tab, index) => (
                   <li key={index} className='transform rotate-90 origin-center'>
                     <Link
                       href={tab.path}
-                      className='text-white font-bold'
+                      className={`${
+                        router.pathname === `${tab.path}`
+                          ? 'text-gold'
+                          : 'text-white font-bold'
+                      }`}
                     >
-                      {tab.name}
+                      {tab.name==='Dashboard' && 
+                      
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M9 1.5H5.625c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0016.5 9h-1.875a1.875 1.875 0 01-1.875-1.875V5.25A3.75 3.75 0 009 1.5zm6.61 10.936a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 14.47a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clip-rule="evenodd" />
+  <path d="M12.971 1.816A5.23 5.23 0 0114.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 013.434 1.279 9.768 9.768 0 00-6.963-6.963z" />
+</svg>
+
+                    }
+                      {tab.name==='Documents' && 
+                      
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                    </svg>
+                    }
+                    {tab.name==='Schedule' && 
+                      
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                      <path d="M12.75 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM7.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM8.25 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM9.75 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM10.5 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM12 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM12.75 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM14.25 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 17.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 15.75a.75.75 0 100-1.5.75.75 0 000 1.5zM15 12.75a.75.75 0 11-1.5 0 .75.75 0 011.5 0zM16.5 13.5a.75.75 0 100-1.5.75.75 0 000 1.5z" />
+                      <path fill-rule="evenodd" d="M6.75 2.25A.75.75 0 017.5 3v1.5h9V3A.75.75 0 0118 3v1.5h.75a3 3 0 013 3v11.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V7.5a3 3 0 013-3H6V3a.75.75 0 01.75-.75zm13.5 9a1.5 1.5 0 00-1.5-1.5H5.25a1.5 1.5 0 00-1.5 1.5v7.5a1.5 1.5 0 001.5 1.5h13.5a1.5 1.5 0 001.5-1.5v-7.5z" clip-rule="evenodd" />
+                    </svg>
+                    
+                    }
+                     {tab.name==='Results' && 
+                      
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                      <path fill-rule="evenodd" d="M5.337 21.718a6.707 6.707 0 01-.533-.074.75.75 0 01-.44-1.223 3.73 3.73 0 00.814-1.686c.023-.115-.022-.317-.254-.543C3.274 16.587 2.25 14.41 2.25 12c0-5.03 4.428-9 9.75-9s9.75 3.97 9.75 9c0 5.03-4.428 9-9.75 9-.833 0-1.643-.097-2.417-.279a6.721 6.721 0 01-4.246.997z" clip-rule="evenodd" />
+                    </svg>
+                    
+                    
+                    }
                     </Link>
                   </li>
                 ))}
             </ul>
           </nav>
         ) : (
-          <nav className='w-3/4 pt-16 pb-2 py-sm:px-0'>
-            <ul className='flex justify-evenly space-x-1 rounded-xl bg-ulm_maroon p-4'>
+          <nav className='w-3/4 pt-16 pb-2 '>
+            <ul className='flex justify-evenly space-x-1 rounded-xl bg-ulm_maroon p-4
+                            '>
               {tabs &&
                 tabs.map((tab, index) => (
                   <li key={index}>
@@ -56,6 +90,7 @@ export default function TabBar({ tabList }) {
                       }`}
                     >
                       {tab.name}
+                      
                     </Link>
                   </li>
                 ))}
